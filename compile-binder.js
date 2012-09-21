c49b84dabded4c7a021433231c15e820ac2c08f3
Kris Kowal <kris.kowal@cixar.com>
2012-09-21 11:56:25 -0700

    Factor binder compilers

--- compile-binder.js
+++ compile-binder.js
@@ -4,24 +4,36 @@ var Observers = require("./observers");
 
 module.exports = compile;
 function compile(syntax) {
-    var args = syntax.args;
-    if (syntax.type === "property") {
-        var observeObject = compileObserver(args[0]);
-        var observeKey = compileObserver(args[1]);
-        return function (observeValue, source, target, parameters) {
-            return observeObject(Observers.autoCancelPrevious(function (object) {
-                return observeKey(Observers.autoCancelPrevious(function (key) {
-                    return observeValue(Observers.autoCancelPrevious(function (value) {
-                        object[key] = value;
-                        return noop;
-                    }), source, parameters);
-                }), target, parameters);
-            }), target, parameters);
-        };
-    } else {
-        throw new Error("Can't compile setter for " + JSON.stringify(syntax));
-    }
+    return compile.semantics.compile(syntax);
 }
 
-function noop() {}
+compile.semantics = {
+
+    compilers: {
+
+        property: function (observeObject, observeKey) {
+            return function (observeValue, source, target, parameters) {
+                return observeObject(Observers.autoCancelPrevious(function (object) {
+                    return observeKey(Observers.autoCancelPrevious(function (key) {
+                        return observeValue(Observers.autoCancelPrevious(function (value) {
+                            object[key] = value;
+                        }), source, parameters);
+                    }), target, parameters);
+                }), target, parameters);
+            };
+        }
+
+    },
+
+    compile: function (syntax) {
+        var compilers = this.compilers;
+        if (compilers.hasOwnProperty(syntax.type)) {
+            var argObservers = syntax.args.map(compileObserver, compileObserver.semantics);
+            return compilers[syntax.type].apply(null, argObservers);
+        } else {
+            throw new Error("Can't compile binder for " + JSON.stringify(syntax));
+        }
+    }
+
+};
 
--- observe.js
+++ observe.js
@@ -3,10 +3,9 @@ var parse = require("./parse");
 var compile = require("./compile-observer");
 
 module.exports = observe;
-
-function observe(object, path, set, parameters) {
+function observe(object, path, callback, parameters) {
     var syntax = parse(path);
     var observe = compile(syntax);
-    return observe(set, object, parameters);
+    return observe(callback, object, parameters);
 }
 
