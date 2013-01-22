d2dc34128efdf89d89cf72165ddffcb85d51afe6
Kris Kowal <kris.kowal@cixar.com>
2013-01-21 23:42:44 -0800

    Elaborate on the evaluate observer spec

--- spec/eval-spec.js
+++ spec/eval-spec.js
@@ -1,6 +1,7 @@
 var Bindings = require("../bindings");
 
 describe("evaluate", function () {
+
     it("should do evil", function () {
 
         var object = Bindings.defineBindings({}, {
@@ -16,5 +17,19 @@ describe("evaluate", function () {
         expect(object.result).toBe(4);
 
     });
+
+    it("should do evil iteratively", function () {
+
+        var object = {};
+        Bindings.defineBindings(object, {
+            "result": {"<-": "source.map{&evaluate($path)}"}
+        }, object);
+
+        object.source = [1, 2, 3];
+        object.path = "*2";
+        expect(object.result).toEqual([2, 4, 6]);
+
+    });
+
 });
 
