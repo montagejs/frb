01117038da4a62046d92ac38742004fed4f80c0f
Stuart Knightley <stuart@stuartk.com>
2013-09-02 21:22:52 -0700

    Add failing spec for sorted block with missing sorted property

--- spec/observe-sorted-spec.js
+++ spec/observe-sorted-spec.js
@@ -26,5 +26,29 @@ describe("sorted block observer", function () {
         expect(object.sorted).toEqual([array[2], array[0], array[1]]);
     });
 
+    it("sorts objects missing the sorted property", function () {
+
+        var array = [
+            {key: 0},
+            {key: 1},
+            {key: 2}
+        ];
+
+        var object = Bindings.defineBindings({
+            array: array
+        }, {
+            sorted: {"<-": "array.sorted{value}"}
+        });
+
+        expect(object.sorted.length).toEqual(3);
+
+        array[2].value = 'a';
+        array[1].value = 'b';
+        array[0].value = 'c';
+
+        expect(object.sorted.length).toEqual(3);
+        expect(object.sorted).toEqual([array[2], array[1], array[0]]);
+    });
+
 });
 
