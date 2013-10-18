5b453994cf0b999c23f906b3f5b6e247d177bceb
Kris Kowal <kris.kowal@cixar.com>
2013-10-17 23:48:03 -0700

    Add min() and max() functions
    These supplement the already existing min{} and max{} blocks, for the
    case where the value to compare is itself.  Both syntaxes work for this
    case now.  Previously, these would fall back to the subtle failure of
    using collection's non-reactive implementation of `min` or `max`.

--- spec/readme-spec.js
+++ spec/readme-spec.js
@@ -322,8 +322,8 @@ describe("Tutorial", function () {
 
     it("Min and Max", function () {
         var object = Bindings.defineBindings({}, {
-            min: {"<-": "values.min{}"},
-            max: {"<-": "values.max{}"}
+            min: {"<-": "values.min()"},
+            max: {"<-": "values.max()"}
         });
 
         expect(object.min).toBe(undefined);
@@ -332,6 +332,9 @@ describe("Tutorial", function () {
         object.values = [2, 3, 2, 1, 2];
         expect(object.min).toBe(1);
         expect(object.max).toBe(3);
+
+        object.values.push(4);
+        expect(object.max).toBe(4);
     });
 
     it("Min and Max (by property)", function () {
