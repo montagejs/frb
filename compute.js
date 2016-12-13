59b86d9377ec724ec567b8e75248e968260ac601
Benoit Marchant <marchant@mac.com>
2016-12-13 14:31:41 -0800

    removes use of .map(), refactor variables

--- compute.js
+++ compute.js
@@ -9,46 +9,65 @@ module.exports = compute;
 function compute(target, targetPath, descriptor) {
     descriptor.target = target;
     descriptor.targetPath = targetPath;
-    var source = descriptor.source = descriptor.source || target;
-    var args = descriptor.args;
-    var compute = descriptor.compute;
-    var parameters = descriptor.parameters = descriptor.parameters || source;
-    var document = descriptor.document;
-    var components = descriptor.components;
-    var trace = descriptor.trace;
+    var source = descriptor.source = descriptor.source || target,
+        args = descriptor.args,
+        compute = descriptor.compute,
+        parameters = descriptor.parameters = descriptor.parameters || source,
+        document = descriptor.document,
+        components = descriptor.components,
+        trace = descriptor.trace;
 
     // TODO consider the possibility that source and target have intrinsic
     // scope properties
     //
-    var sourceScope = descriptor.sourceScope = new Scope(source);
+    var sourceScope = /* descriptor.sourceScope =*/ new Scope(source);
     sourceScope.parameters = parameters;
     sourceScope.document = document;
     sourceScope.components = components;
-    var targetScope = descriptor.targetScope = new Scope(target);
+
+    var targetScope = /*descriptor.targetScope =*/ new Scope(target);
     targetScope.parameters = parameters;
     targetScope.document = document;
     targetScope.components = components;
 
-    var argObservers = args.map(function (arg) {
-        return parse(arg);
-    }).map(function (argSyntax) {
-        if (argSyntax.type === "rangeContent") {
-            var observeArg = compileObserver(argSyntax.args[0]);
-            return Observers.makeRangeContentObserver(observeArg);
-        } else if (argSyntax.type === "mapContent") {
-            var observeArg = compileObserver(argSyntax.args[0]);
-            return Observers.makeMapContentObserver(observeArg);
-        } else {
-            return compileObserver(argSyntax);
-        }
-    });
-    var argsObserver = Observers.makeRangeContentObserver(
-        Observers.makeObserversObserver(argObservers)
-    );
-    var observeSource = Observers.makeComputerObserver(argsObserver, compute, target);
 
-    var targetSyntax = parse(targetPath);
-    var bindTarget = compileBinder(targetSyntax);
+    var argObservers = [];
+    for(var i = 0, arg, argSyntax, observeArg, iObserver;(arg = args[i]); i++) {
+        argSyntax = parse(arg);
+        if (argSyntax.type === "rangeContent") {
+            observeArg = compileObserver(argSyntax.args[0]);
+            iObserver = Observers.makeRangeContentObserver(observeArg);
+        } else if (argSyntax.type === "mapContent") {
+            observeArg = compileObserver(argSyntax.args[0]);
+            iObserver = Observers.makeMapContentObserver(observeArg);
+        } else {
+            iObserver = compileObserver(argSyntax);
+        }
+        argObservers.push(iObserver);
+    }
+    // var argObservers = args.map(function (arg) {
+    //     return parse(arg);
+    // }).map(function (argSyntax) {
+    //     if (argSyntax.type === "rangeContent") {
+    //         var observeArg = compileObserver(argSyntax.args[0]);
+    //         return Observers.makeRangeContentObserver(observeArg);
+    //     } else if (argSyntax.type === "mapContent") {
+    //         var observeArg = compileObserver(argSyntax.args[0]);
+    //         return Observers.makeMapContentObserver(observeArg);
+    //     } else {
+    //         return compileObserver(argSyntax);
+    //     }
+    // });
+
+
+
+    var argsObserver = Observers.makeRangeContentObserver(
+            Observers.makeObserversObserver(argObservers)
+        ),
+        observeSource = Observers.makeComputerObserver(argsObserver, compute, target),
+
+        targetSyntax = parse(targetPath),
+        bindTarget = compileBinder(targetSyntax);
 
     return bindTarget(observeSource, sourceScope, targetScope, descriptor, trace ? {
         sourcePath: args.join(", "),
